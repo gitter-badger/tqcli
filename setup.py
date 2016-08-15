969415a162f1a335bec1df74fb7834b587c5e292
Mehrdad Pazooki <mehrdad@tranquant.com>
2016-08-14 22:54:17 -0400

    adding setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ requirements = [l.split('=')[0] for l in open('requirements.txt', 'r').read().sp
 
 setup(
     name='tqcli',
-    version='0.1',
+    version='1.0',
     description='TQCLI is the client application to use TranQuant services',
     url='http://github.com/tranquant/tqcli',
     author='Mehrdad Pazooki, Sean Glover, Rodrigo Abreu',
     author_email='mehrdad@tranquant.com',
     license='Apache 2.0',
     install_requires=requirements,
-    packages=['tqcli'],
+    packages=['tqcli', 'tqcli/batch', 'tqcli/config'],
     zip_safe=True
 )
